084d4bcf7939cfb9b660ee22460eea8ca43cf372
Walter <2760walter@gmail.com>
2024-04-02 11:22:13 +0300

    get meme

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import   json
+import  requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,20 @@ app = Flask(__name__)
 @app.route('/welcome')
 def welcome():
     return 'Welcome to the Web Programming'
+
+def  get_meme() :
+    #  url = "https://api.imgflip.com/get_memes"
+     url = "https://meme-api.com/gimme"
+     response = json.loads(requests.request('GET',url).text)
+     meme_large =response['preview'][-2]
+     subreddit = response['subreddit']
+     return meme_large, subreddit
+
+@app.route('/')
+def index():
+    
+    meme_pic, subreddit = get_meme()
+    return render_template('index.html',  meme_pic=meme_pic, subreddit =subreddit)
 
 
 if __name__ == '__main__':
